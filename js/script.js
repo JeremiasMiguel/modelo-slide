1326c98304f573b5048bcc2dff81b1f3982074e8
Jeremias Miguel <jjjeremiasmiguel@gmail.com>
2020-11-05 18:14:47 -0300

    Custom controls estilizados

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper");
 
 slide.init();
 slide.addArrow(".prev", ".next");
 
-slide.addControl();
+// Adicionando um custom control, agora a navegação muda de bolinhas para imagens 
+// equivalentes, já captando o index e fazendo com que a navegação inferior suma
+slide.addControl(".custom-controls");
